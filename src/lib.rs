0d91f864a78b77bc4f7a0292ef4a5750746ccdf4
Reyesjessica <jesi@JESSICA>
2025-04-20 23:22:27 -0600

    modicacion de lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,42 @@
 #![no_std]
-use soroban_sdk::{contractimpl, Env, Symbol, Map, Address};
+use soroban_sdk::{contractimpl, symbol_short, Address, Env, Map, Symbol};
 
-pub struct LecturaEscritura;
-
-#[derive(Clone)]
-pub struct Registro {
-    lecturas: u32,
-    escrituras: u32,
-}
+pub struct MiContratoRW;
 
 #[contractimpl]
-impl LecturaEscritura {
-    // Inicializa un nuevo registro para un usuario
+impl MiContratoRW {
     pub fn iniciar_usuario(env: Env, usuario: Address) {
         let datos = Map::<Symbol, u32>::new(&env);
         env.storage().instance().set(&usuario, &datos);
     }
 
-    // Registrar una lectura
     pub fn registrar_lectura(env: Env, usuario: Address) {
-        let mut datos: Map<Symbol, u32> = env.storage().instance().get(&usuario).unwrap_or_default();
-        let count = datos.get(Symbol::short("lecturas")).unwrap_or(0);
-        datos.set(Symbol::short("lecturas"), count + 1);
+        let mut datos = env
+            .storage()
+            .instance()
+            .get::<Address, Map<Symbol, u32>>(&usuario)
+            .unwrap_or_else(|| Map::new(&env));
+        let count = datos.get(symbol_short!("lectura")).unwrap_or(0);
+        datos.set(symbol_short!("lectura"), count + 1);
         env.storage().instance().set(&usuario, &datos);
     }
 
-    // Registrar una escritura
     pub fn registrar_escritura(env: Env, usuario: Address) {
-        let mut datos: Map<Symbol, u32> = env.storage().instance().get(&usuario).unwrap_or_default();
-        let count = datos.get(Symbol::short("escrituras")).unwrap_or(0);
-        datos.set(Symbol::short("escrituras"), count + 1);
+        let mut datos = env
+            .storage()
+            .instance()
+            .get::<Address, Map<Symbol, u32>>(&usuario)
+            .unwrap_or_else(|| Map::new(&env));
+        let count = datos.get(symbol_short!("escritura")).unwrap_or(0);
+        datos.set(symbol_short!("escritura"), count + 1);
         env.storage().instance().set(&usuario, &datos);
     }
 
-    // Obtener estado de un usuario
     pub fn obtener_registro(env: Env, usuario: Address) -> Map<Symbol, u32> {
-        env.storage().instance().get(&usuario).unwrap_or_default()
+        env.storage()
+            .instance()
+            .get::<Address, Map<Symbol, u32>>(&usuario)
+            .unwrap_or_else(|| Map::new(&env))
     }
 }
+
