6e782074772680762a69467c6ba28ced863f2e01
Antonia2410 <22620025@tlaxiaco.tecnm.mx>
2025-04-21 13:58:24 -0600

    Eliminar Usuario

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 #![no_std]
 use soroban_sdk::{contractimpl, symbol_short, Address, Env, Map, Symbol};
 
+// Definición de la estructura del contrato
 pub struct MiContratoRW;
 
 #[contractimpl]
 impl MiContratoRW {
+    /// Inicializa el usuario creando un mapa vacío asociado al usuario en el almacenamiento.
     pub fn iniciar_usuario(env: Env, usuario: Address) {
         let datos = Map::<Symbol, u32>::new(&env);
         env.storage().instance().set(&usuario, &datos);
     }
 
+    /// Registra una lectura incrementando el contador correspondiente.
     pub fn registrar_lectura(env: Env, usuario: Address) {
         let mut datos = env
             .storage()
@@ -21,6 +24,7 @@ impl MiContratoRW {
         env.storage().instance().set(&usuario, &datos);
     }
 
+    /// Registra una escritura incrementando el contador correspondiente.
     pub fn registrar_escritura(env: Env, usuario: Address) {
         let mut datos = env
             .storage()
@@ -32,11 +36,17 @@ impl MiContratoRW {
         env.storage().instance().set(&usuario, &datos);
     }
 
+    /// Obtiene el registro completo asociado a un usuario, incluyendo lecturas y escrituras.
     pub fn obtener_registro(env: Env, usuario: Address) -> Map<Symbol, u32> {
         env.storage()
             .instance()
             .get::<Address, Map<Symbol, u32>>(&usuario)
             .unwrap_or_else(|| Map::new(&env))
+    }
+
+    /// Elimina el registro de un usuario del almacenamiento, si existe.
+    pub fn eliminar_usuario(env: Env, usuario: Address) {
+        env.storage().instance().remove(&usuario);
     }
 }
 
